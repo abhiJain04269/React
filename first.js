b37980758bc58f0ff5d0870a62a60d954796a6ec
Abhi Jain <abhijain1722@gmail.com>
2025-01-21 23:14:40 +0530

    commited the code

---
+++ first.js
@@ -0,0 +1,2 @@
+const element= React.createElement("h1",{id:"first_heading",className:"heading",style:{color:"white",backgroundColor:"purple",fontSize:"40px"}},"Hello coder army");
+ReactDOM.createRoot(document.getElementsByClassName("root")[0]).render(element);
